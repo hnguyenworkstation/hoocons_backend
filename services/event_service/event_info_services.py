fa048863e0296de3ecb3244128b626f53f3969dc
Hung Nguyen <hnguyenworkstation@gmail.com>
2017-06-12 10:53:39 -0400

    update action when create new event

--- services/event_service/event_info_services.py
+++ services/event_service/event_info_services.py
@@ -2,9 +2,11 @@ from datetime import datetime
 
 from flask_jwt import jwt_required, current_identity
 from flask_restful import reqparse, Resource
+from static import app_constant
 
 import static.status as status
 from models.base_event import BaseEvent
+from models.action import BaseAction
 
 
 class CreateEventRequest(Resource):
@@ -29,7 +31,13 @@ class CreateEventRequest(Resource):
                               images=body.images, contain_event=body.contain_event,
                               privacy=body.privacy, location=[body.longitude, body.latitude],
                               tags=body.tags).save()
-            user.update(add_to_set__posted_events=event, last_online=datetime.utcnow())
+
+            # Now create an action
+            action = BaseAction(by_user=user, action_type=app_constant.action_create_new_event, target=event.id,
+                                action_priority=app_constant.priority_medium).save()
+            user.update(add_to_set__posted_events=event, add_toset__recent_actions=action,
+                        last_online=datetime.utcnow())
+
             return event.get_initial_json(), status.HTTP_200_OK
         except Exception as e:
             return {"error": str(e)}, status.HTTP_400_BAD_REQUEST
