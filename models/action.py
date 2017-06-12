16ebab040cffbed9b9357e87d525a7224b535588
Hung Nguyen <hnguyenworkstation@gmail.com>
2017-06-12 09:09:09 -0400

    updated base action

--- models/action.py
+++ models/action.py
@@ -1,11 +1,6 @@
 from datetime import datetime
 
 from mongoengine import *
-
-from flask_jwt import jwt_required, current_identity
-from flask_restful import reqparse, Resource
-from models.user import User
-
 from static import app_constant
 
 
@@ -15,16 +10,27 @@ class BaseAction(Document):
         @action_type = LIKE, SHARE, MAKING FRIENDS, POST EVENT ....
         @target: Id of target (event, user)
         @with_content: Maybe NONE since content only available for comment
+        @action_priority: this field determine how we order the list of events shows on homepage
     """
     by_user = ReferenceField('User', required=True)
     action_type = StringField(default="", choices=app_constant.ACTION_TYPES)
+    date_of_action = DateTimeField(default=datetime.utcnow())
     target = StringField(default="")
     with_content = StringField(default="")
+    action_priority = StringField(default="", choices=app_constant.ACTION_PRIORITY)
+    is_scrolled_by = BooleanField(default=False)
+    is_viewed = BooleanField(default=False)
 
     def get_complete_json(self):
+        target = Document.objects(id=self.target).first()
+        with_content = Document.objects(id=self.with_content).first()
+
         return {
             "id": self.id,
             "by_user": self.by_user.get_simple_header(),
-            "target": self.target.get_complete_json(),
-            "with_content": self.get_complete_json()
+            "target": "" if target is None else target.get_complete_json(),
+            "with_content": "" if with_content is None else with_content.get_complete_json(),
+            "action_priority": self.action_priority,
+            "is_scrolled_by": self.is_scrolled_by,
+            "is_viewed": self.is_viewed
         }
--- static/app_constant.py
+++ static/app_constant.py
@@ -16,7 +16,9 @@ is_friend = "IS_FRIEND"
 friend_requesting = "REQUESTING"
 user_blocked = "BLOCKED"
 
-# Action type constant
+'''
+    # Action types and priorities
+'''
 ACTION_TYPES = ["CREATE_NEW_EVENT", "LIKE_EVENT", "COMMENT_EVENT", "SHARE_EVENT", "MAKING_FRIEND", "EVENT_AD",
                 "POSTER_AD"]
 action_create_new_event = "CREATE_NEW_EVENT"
@@ -26,3 +28,11 @@ action_share_event = "SHARE_EVENT"
 action_making_friend = "MAKING_FRIEND"
 action_event_ad = "EVENT_AD"
 action_post_ad = "POSTER_AD"
+
+ACTION_PRIORITY = ["PRIORITY_LOW", "PRIORITY_MEDIUM", "PRIORITY_HIGH", "PRIORITY_SUPER_HIGH"]
+priority_low = "PRIORITY_LOW"
+priority_medium = "PRIORITY_MEDIUM"
+priority_high = "PRIORITY_HIGH"
+priority_super_high = "PRIORITY_SUPER_HIGH"
+
+
