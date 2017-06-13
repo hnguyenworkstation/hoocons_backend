fe37216c793bc8aadd610db273e0d646afdba042
Hung Nguyen <hnguyenworkstation@gmail.com>
2017-06-13 14:30:27 -0400

    added view count and last action field fo thread

--- models/thread.py
+++ models/thread.py
@@ -5,18 +5,20 @@ from models.thread_reply import BaseThreadReply
 
 
 class BaseThread(Document):
+
     """#################################################
-        Field requirements:
-            + created_by: user who created this thread
-            + created_at: when was this thread created
-            + title: thread title
-            + text_content: content written among with the title
-            + images: images attached
-            + tags: those tags thing that user mention in the thread
-            + replies: what people reply into this thread
-            + is_blocked: is this thread blocked by admin (not allow to comment....)
-            + is_pinned: is this thread pinned into a channel
+    Field requirements:
+        + created_by: user who created this thread
+        + created_at: when was this thread created
+        + title: thread title
+        + text_content: content written among with the title
+        + images: images attached
+        + tags: those tags thing that user mention in the thread
+        + replies: what people reply into this thread
+        + is_blocked: is this thread blocked by admin (not allow to comment....)
+        + is_pinned: is this thread pinned into a channel
     #################################################"""
+
     channel_owner = ReferenceField('BaseChannel', default="", required=True)
     created_by = ReferenceField('User', required=True)
     created_at = DateTimeField(default=datetime.utcnow())
@@ -27,6 +29,8 @@ class BaseThread(Document):
     tags = ListField(StringField(), default=[])
 
     replies = ListField(ReferenceField(BaseThreadReply), default=[])
+    view_count = IntField(default=1)
+    last_action = DateTimeField(default=datetime.utcnow())
 
     is_blocked = BooleanField(default=False)
     is_pinned = BooleanField(default=False)
